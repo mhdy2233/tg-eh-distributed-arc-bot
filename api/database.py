c548f8215049cc7d7a9ab12d30276d464d206014
mhdy2233 <mhdy2233@foxmail.com>
2025-04-17 21:13:59 +0800

    modified:   api/database.py

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 替换下面的内容为你的真实数据库配置
-DATABASE_URL = "mysql+pymysql://root:mhdy2233@77.90.41.131:3306/eh_arc"
+DATABASE_URL = "mysql+pymysql://root:pass@127.0.0.1:3306/eh_arc"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
