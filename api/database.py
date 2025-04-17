ef92aa81bdf9a9192be6fbd068b5790175a7e9b6
mhdy2233 <mhdy2233@foxmail.com>
2025-04-17 21:16:59 +0800

    sb

--- api/database.py
+++ api/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import yaml
+
+with open("./config.yml", 'r', encoding='utf-8') as f:
+    config = yaml.safe_load(f)
 
 # 替换下面的内容为你的真实数据库配置
-DATABASE_URL = "mysql+pymysql://root:pass@127.0.0.1:3306/eh_arc"
+DATABASE_URL = f"mysql+pymysql://{config['mysql']['user']}:{config['mysql']['password']}@{config['mysql']['host']}:{config['mysql']['port']}/{config['mysql']['db']}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
